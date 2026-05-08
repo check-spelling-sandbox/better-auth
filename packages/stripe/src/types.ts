74b19140f660f0fe6af3b354ad963a1c85a09180
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-05-08 08:32:03 -0400

    spelling: it
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- packages/stripe/src/types.ts
+++ packages/stripe/src/types.ts
@@ -233,7 +233,7 @@ export interface Subscription {
 	 */
 	canceledAt?: Date | undefined;
 	/**
-	 * If the subscription has ended, the date the subscription ended.
+	 * If the subscription has ended, the date it ended.
 	 */
 	endedAt?: Date | undefined;
 	/**
